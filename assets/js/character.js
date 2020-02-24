544bf8f686d6e2629d6d47e1f2e77fc5d96fe97d
Drubaloo <Drubaloo@outlook.com>
2020-02-24 09:10:45 -0800

    backing up

---
+++ assets/js/character.js
@@ -0,0 +1,17 @@
+var character = {
+    race: ``,
+    class: ``,
+    alignment: ``,
+    strength:  ``,
+    dexterity: ``,
+    constitution: ``,
+    intelligence: ``,
+    wisdom: ``,
+    charisma: ``,
+    armorClass: ``,
+    initative: ``,
+    speed: ``,
+    languages: [],
+    equipment: ``,
+
+}
---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+var races = `https://www.dnd5eapi.co/api/races`
+var classes = `https://www.dnd5eapi.co/api/classes`
