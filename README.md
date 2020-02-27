3732b333cc7eaf597cecb9795370cd7e3d41038e
Drubaloo <Drubaloo@outlook.com>
2020-02-26 17:32:15 -0800

    information update

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Dungeons-Dragons
-Making a character generator
+# Dungeons-Dragons 🐲🏯🐲
+
+>A useful way to make and maintain a character in Dungeons and Dragons
+
+## 💻What I used💽
+HTML5, CSS, Bootstrap, JavaScript
+
+## Update Note (Feb 21, 2020)
+- Added Skeleton HTML and CSS
+
+## Update Note (Feb 24, 2020)
+- Added JS to dynamically create buttons for each race
+
+## Update Note (Feb 26, 2020)
+- Dynamically added ability information for each race
+
+
+## I love getting feedback and support! 🎆🎇🎈
+
+
+## 🕹🕹🎮 Preview
+
+![Image of Preview](/assets/images/Screenshot.png)
+
+
+
+##Author
+
+**🏍Dru Sanchez🏍**
+*Twitter: [ImNotDrumatic](https://twitter.com/ImNotDrumatic)
+*Github:  [DruSanchez](https://github.com/Drubaloo)
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,55 +6,108 @@ var classes = `https://www.dnd5eapi.co/api/classes`
 $.ajax({
     url: races,
     method: `GET`,
+
 }).then(function (raceResponse) {
     //dynamically generate a button for each race
     for (var i = 0; i < raceResponse.results.length; i++) {
 
-        var raceID = raceResponse.results[i].index
-        var raceName = raceResponse.results[i].name
-        //create button
+        var raceResult = raceResponse.results[i];
+
         var a = $(`<button>`)
 
         //add a class to each button
-        a.addClass('btn btn-primary btn-block m-1 race')
-        //add data attr
-        a.attr(`id`, raceResponse.results[i].index)
+        a.addClass('btn btn-primary btn-block m-1 race ')
+        //add attributes
+        a.attr(`id`, raceResult.index)
+        a.attr(`data-toggle`, `dropdown`)
+        a.attr(`aria-haspopup`, `true`)
+        a.attr(`aria-expanded`, `false`)
         //give the button some text
-        a.text(raceResponse.results[i].name)
+        a.text(raceResult.name)
         //show the button
         $(`#buttons-view`).append(a)
 
         $.ajax({
-            url: races + `/` + raceResponse.results[i].index,
+            url: races + `/` + raceResult.index,
             method: `GET`,
+
         }).then(function (raceInfo) {
+
+            //create button
+
+            var raceID = raceInfo.index
             console.log(raceInfo)
+            console.log(raceID)
             // add info to button on hover
             var b = $(`<p>`)
             var c = $(`<p>`)
             var d = $(`<p>`)
+            var e = $(`<p>`)
+            var f = $(`<p>`)
+            var g = $(`<p>`)
+            var h = $(`<p>`)
             //add a class to show div
             b.addClass(`information`)
             c.addClass(`information`)
             d.addClass(`information`)
+            d.addClass(`information`)
+            e.addClass(`information`)
+            f.addClass(`information`)
+            g.addClass(`information`)
+            h.addClass(`information`)
             //add data attribute
             b.attr(`info`)
             c.attr(`info`)
             d.attr(`info`)
+            e.attr(`info`)
+            f.attr(`info`)
+            g.attr(`info`)
+            h.attr(`info`)
+
+
             //give the information some content
             b.text('Speed: ' + raceInfo.speed + "\n")
-            for (var j = 0; j < raceInfo.ability_bonuses.length; j++) {
-                c.text(raceInfo.ability_bonuses[j].name + `+ ` + raceInfo.ability_bonuses[j].bonus)
-                d.text(raceInfo.ability_bonuses[j].name + `+ ` + raceInfo.ability_bonuses[j].bonus)
 
-                //append the information to each button
+            if (raceID == `dragonborn` || raceID == `dwarf` || raceID == `elf` || raceID == `gnome` || raceID == `half-elf` || raceID == `half-orc` || raceID == `halfling` || raceID == `human` || raceID == `tiefling`) {
 
-                $('#' + raceID).append(b)
-                $('#' + raceID).append(c)
-                $('#' + raceID).append(d)
+                c.text(raceInfo.ability_bonuses[0].name + `+ ` + raceInfo.ability_bonuses[0].bonus)
+
+
+
             }
+
+            if (raceID == `dragonborn` || raceID == `half-orc` || raceID == `human` || raceID == `tiefling`) {
+
+                d.text(raceInfo.ability_bonuses[1].name + `+ ` + raceInfo.ability_bonuses[1].bonus)
+            }
+
+            if (raceID == `human`) {
+
+                e.text(raceInfo.ability_bonuses[2].name + `+ ` + raceInfo.ability_bonuses[2].bonus)
+                f.text(raceInfo.ability_bonuses[3].name + `+ ` + raceInfo.ability_bonuses[3].bonus)
+                g.text(raceInfo.ability_bonuses[4].name + `+ ` + raceInfo.ability_bonuses[4].bonus)
+                h.text(raceInfo.ability_bonuses[5].name + `+ ` + raceInfo.ability_bonuses[5].bonus)
+
+            }
+
+
+
+            //     //append the information to each button
+            $('#' + raceID).append(b)
+            $('#' + raceID).append(c)
+            $('#' + raceID).append(d)
+            $('#' + raceID).append(e)
+            $('#' + raceID).append(f)
+            $('#' + raceID).append(g)
+            $('#' + raceID).append(h)
+
+
+
+
+
         })
 
 
     }
 })
+
