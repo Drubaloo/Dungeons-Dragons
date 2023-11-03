86d5e8fc25aa3794f17f1f5119685ed44cead8b1
Dru Sanchez <drubaloo@Outlook.com>
2023-11-03 11:50:34 -0700

    updating Equipment schema

--- server/models/Equipment.js
+++ server/models/Equipment.js
@@ -1,5 +1,18 @@
 const { Schema, model } = require("mongoose")
 
+const categorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    }
+)
+
 const equipmentSchema = new Schema({
     index: {
         type: String,
@@ -13,6 +26,8 @@ const equipmentSchema = new Schema({
         type: String,
         required: true
     },
+    category: categorySchema,
+    weight: Number
 })
 
 const Equipment = model("Equipment", equipmentSchema)
